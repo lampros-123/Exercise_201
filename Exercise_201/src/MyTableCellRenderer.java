42d138e4f7525afba91617f5993b774df131e067
neukoc16 <neukoc16@new.edvhtl.at>
2018-10-04 08:51:11 +0200

    added label color

--- Exercise_201/src/MyTableCellRenderer.java
+++ Exercise_201/src/MyTableCellRenderer.java
@@ -15,15 +15,18 @@ public class MyTableCellRenderer implements TableCellRenderer {
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Measurement m = (Measurement) value;
-        JLabel label=new JLabel();
+        JLabel label = new JLabel();
         label.setOpaque(true);
-        if(m.getMeasured()>30){
-            
+        if (m.getMeasured() > 30) {
+            label.setBackground(Color.RED);
+        } else if (m.getExceedence() <= 30 && m.getExceedence() > 20) {
+            label.setBackground(Color.ORANGE);
+        } else if (m.getExceedence() <= 20 && m.getExceedence() > 10) {
+            label.setBackground(Color.YELLOW);
+        } else if (m.getExceedence() <= 10) {
+            label.setBackground(Color.BLUE);
         }
-        else if(m.getExceedence()<=30&&m.getExceedence()>20){
-            
-        }esle if(m.getExceedence())
-            
+
     }
 
 }
